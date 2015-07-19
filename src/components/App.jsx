48d5abc5404b204ae35be818feea64f0049af16f
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-07-19 13:32:45 +0800

    Lint App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,26 +3,25 @@ import "../styles/style";
 
 import * as someContent from "./content.jsx";
 
-import React from "react"
+import React from "react";
 
 class TodoApp extends React.Component {
 
   constructor(props) {
     super(props);
-    this.onClickHandler = this.onClickHandler.bind(this);
     this.state = {
       date: this.theTime(),
-      msg: "State says the time is:",
-      title: props.title
+      msg: "State says the time is:"
     };
+    this.onClickHandler = this.onClickHandler.bind(this);
   }
 
   theTime() {
-    return (new Date).toLocaleTimeString()
+    return (new Date()).toLocaleTimeString();
   }
 
-  onClickHandler(e) {
-    this.setState({date: this.theTime()})
+  onClickHandler() {
+    this.setState({date: this.theTime()});
   }
 
   render() {
@@ -32,13 +31,13 @@ class TodoApp extends React.Component {
         <br />
         {this.state.msg} {this.state.date}
         <br />
-        {this.state.title}
+        {this.props.title}
       </div>
-    )
+    );
   }
 }
 
 React.render(
   <TodoApp title="I am a property!" />,
   document.getElementById("todo-app")
-)
+);
