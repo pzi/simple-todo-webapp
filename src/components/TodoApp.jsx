2a11239510d1d0cefcf1355cbbd44acd514a968e
Levi Buzolic <levibuzolic@gmail.com>
2015-07-19 18:47:36 +1000

    Such config

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,44 +2,60 @@ import 'normalize.css';
 import '../styles/style';
 
 import React from 'react';
-import content from './content';
-import ES6Component from 'components/examples/ES6Component';
-import ES6StandardComponent from 'components/examples/ES6StandardComponent';
-import ES5Component from 'components/examples/ES5Component';
+import request from 'axios';
 
 export default class TodoApp extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { tick: 0 };
+    this.state = {
+      todos: null
+    };
   }
 
   componentDidMount() {
-    this._interval = setInterval(this.tick.bind(this), 1000);
+    // Fake TODOs API
+    request
+      .get('http://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        this.onLoadTodos(response.data)
+      })
+      .catch((response) => {
+        console.log('Error:', response)
+      })
   }
 
-  componentWillUnmount() {
-    clearInterval(this._interval);
-  }
-
-  tick() {
-    this.setState({ tick: this.state.tick + 1 });
+  onLoadTodos(todos) {
+    this.setState({ todos: todos });
   }
 
   render() {
-    const date = (new Date()).toLocaleTimeString();
-
     return (
       <div>
-        <h1>{ this.props.title }</h1>
-        { content }
-        <div>
-          The time is: { date }
-        </div>
-        <ES6Component title="Test" initialCount={ 123 } />
-        <ES6StandardComponent />
-        <ES5Component />
+        <h1>TodoApp</h1>
+        { this.renderTodos() }
       </div>
+    );
+  }
+
+  renderTodos() {
+    if (this.state.todos == null) return <div>Loading...</div>;
+
+    return this.state.todos.length > 0 ? (
+      <ol>
+        { this.state.todos.map(this.renderTodo) }
+      </ol>
+    ) : (
+      <div>Sorry, no TODOs for you.</div>
+    )
+  }
+
+  renderTodo(todo) {
+    return (
+      <li key={ todo.id }>
+        { todo.completed ? '✅' : '❎' }
+        { todo.title }
+      </li>
     );
   }
 }
