109715e6245520c34db25a4f735840c7e9f5c1c0
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-07-26 17:37:35 +0800

    Change from double to single quotes

--- src/application.jsx
+++ src/application.jsx
@@ -1,11 +1,11 @@
 // Manually load ES5 Compat.
-import 'es5-shim/es5-shim'
-import 'es5-shim/es5-sham'
+import 'es5-shim/es5-shim';
+import 'es5-shim/es5-sham';
 
-import React from "react"
-import TodoApp from "components/TodoApp"
+import React from 'react';
+import TodoApp from 'components/TodoApp';
 
 React.render(
-  <TodoApp title="I am a property!" />,
-  document.getElementById("todo-app")
-)
+  <TodoApp title='I am a property!' />,
+  document.getElementById('todo-app')
+);
