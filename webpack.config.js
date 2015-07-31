4b9a7d243acdbc6d2a2d8ff53a3074fea5ab7390
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-07-31 10:12:31 +0800

    Add DedupePlugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -92,6 +92,7 @@ module.exports = {
 
   plugins: isProduction ? [
     new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new CompressionPlugin()
   ] : [
