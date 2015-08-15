e33e18b99abff413444ced8cf34b7a028152f73f
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-08-15 09:55:23 +0800

    Simplify TodoCount

--- src/components/TodoCount.jsx
+++ src/components/TodoCount.jsx
@@ -3,45 +3,29 @@ import React from 'react';
 export default React.createClass({
   displayName: 'TodoCount',
 
-  getInitialState: function() {
-    return {
-      count: 0,
-      completedCount: 0,
-      active: 0
-    };
-  },
-
   propTypes: {
     todos: React.PropTypes.array
   },
 
-  componentDidMount: function() {
-    this._count(this.props.todos);
-  },
-
-  componentWillReceiveProps: function() {
-    this._count(this.props.todos);
-  },
-
   _count: function(todos) {
     const completed = todos.filter((todo) => todo.completed).length;
-
-    this.setState({
-      count: this.props.todos.length,
-      completedCount: completed,
-      active: this.props.todos.length - completed
-    });
+    return {
+      completed: completed,
+      active: todos.length - completed
+    };
   },
 
   render: function () {
+    const todos = this.props.todos;
+
     return (
       <dl>
         <dt>Number of Todos:</dt>
-        <dd>{ this.state.count }</dd>
+        <dd>{ todos.length }</dd>
         <dt>Active Todos:</dt>
-        <dd>{ this.state.active }</dd>
+        <dd>{ this._count(todos).active }</dd>
         <dt>Completed Todos:</dt>
-        <dd>{ this.state.completedCount }</dd>
+        <dd>{ this._count(todos).completed }</dd>
       </dl>
     );
   }
