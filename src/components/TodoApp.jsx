0d7320c6b88a373c8c5e66418f9e17333d3138b5
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-08-21 23:07:05 +0800

    Add new item input

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -7,6 +7,8 @@ import TodoItems from 'components/TodoItems';
 import TodoCount from 'components/TodoCount';
 
 const JSON_PLACEHOLDER_URL = 'http://jsonplaceholder.typicode.com';
+const ENTER_KEY = 13;
+
 export default React.createClass({
   displayName: 'TodoApp',
 
@@ -55,6 +57,34 @@ export default React.createClass({
       });
   },
 
+  _handleKeyDown: function(event) {
+    if (event.keyCode !== ENTER_KEY) { return; }
+
+    event.preventDefault();
+
+    const todos = this.state.todos;
+    const newTodoField = React.findDOMNode(this.refs.newTodo);
+    const newTodoText = newTodoField.value.trim();
+
+    if(newTodoText) {
+      request
+        .post(`${JSON_PLACEHOLDER_URL}/todos`, {
+          'userId': Math.floor(Math.random() * 10),
+          'title': newTodoText,
+          'completed': false
+        })
+        .then((response) => {
+          todos.push(response.data);
+          this.setState({ todos: todos });
+          newTodoField.value = '';
+        })
+        .catch((response) => {
+          alert('Check console!');
+          console.log('Error: ', response);
+        });
+    }
+  },
+
   _renderTodoCount: function() {
     return <TodoCount todos={ this.state.todos } />;
   },
@@ -64,6 +94,7 @@ export default React.createClass({
 
     return (
       <div>
+        <input onKeyDown={ this._handleKeyDown } ref='newTodo' placeholder='What&apos;s next?' />
         { this._renderTodoCount() }
         <TodoItems todos={ this.state.todos } onChange={ this._handleChange } />
         { this._renderTodoCount() }
